08f968b77bdce0302be6956cc49c0a8cfdfa9a69
Nilesh Gupta <nileshgupta2797@gmail.com>
2024-04-21 13:48:55 -0500

    complex math blocks and preprocess

--- jupyter2tex.py
+++ jupyter2tex.py
@@ -1,5 +1,5 @@
 from jupyter_notebook_parser import JupyterNotebookParser
-import re
+import re, os
 
 parsed = JupyterNotebookParser('sample.ipynb')
 all_markdown_lines = [y for x in parsed.get_markdown_cell_sources() for y in x.split('\n')]
@@ -17,6 +17,7 @@ def count_spaces(l):
             break
         first += 1
     return ret // 4, l[first:]
+
 def enumerate(line):
     global enum_i
     pre = []
@@ -51,6 +52,7 @@ def enumerate(line):
     
     # ELSE
     return ''.join(pre) + line
+
 def preprocess(line):
     # replace any *_* with *\_*
     line = line.replace('_','\_')
@@ -64,12 +66,29 @@ def preprocess(line):
 
     # DONE
     return line
+
+def split_on_dollar_signs(text):
+    pattern = r'(\${1,2}.*?\${1,2})'
+    parts = re.split(pattern, text)
+    return [part for part in parts if part.strip()]
+
+def split_on_verbatim_signs(text):
+    pattern = r'(\`{1,3}.*?\`{1,3})'
+    parts = re.split(pattern, text)
+    return [part for part in parts if part.strip()]
+
+def is_part_of_special_block(line, began_math, began_verbatim):
+    return line.startswith('```') or line.startswith('$') or line.startswith('$$') or line.startswith('```') or began_math or began_verbatim
+
 def markdown_to_latex(lines):
     latex_lines = ['\\documentclass{article}\n\\usepackage{graphicx}\n\\begin{document}']
-    began_verbatim = False
+    began_verbatim = False; began_math = False
+    lines = [y.strip() for x in lines for y in split_on_dollar_signs(x)]
+    lines = [y.strip() for x in lines for y in split_on_verbatim_signs(x)]
     for line in lines:
-        if not began_verbatim:
+        if not is_part_of_special_block(line, began_math, began_verbatim):
             line = preprocess(line)
+
         # Convert headers
         if line.startswith('# '):
             latex_lines.append('\\section{' + line[2:] + '}')
@@ -78,13 +97,19 @@ def markdown_to_latex(lines):
         elif line.startswith('### '):
             latex_lines.append('\\subsubsection{' + line[4:] + '}')
         # Convert code blocks
-        elif line.startswith('```'):
+        elif line == '```':
             if latex_lines and began_verbatim:
                 latex_lines.append('\\end{verbatim}')
                 began_verbatim = False
             else:
                 latex_lines.append('\\begin{verbatim}')
                 began_verbatim = True
+        # Inline code
+        elif line[0] == '`' and line[-1] == '`':
+            latex_lines.append('\\texttt{' + line.replace('`', '') + '}')
+        # Math blocks
+        elif line == '$' or line == '$$':
+            began_math = not began_math
         else:
             latex_lines.append(line)
     latex_lines.append('\\end{document}')
@@ -93,3 +118,4 @@ def markdown_to_latex(lines):
 # RUN ON SAMPLE
 latex_lines = markdown_to_latex(all_markdown_lines)
 print(*latex_lines, sep='\n', file=open('latex.tex', 'w'))
+os.system('pdflatex latex.tex')
