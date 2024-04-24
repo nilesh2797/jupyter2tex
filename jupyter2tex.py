9c6ce9c96845c7b61ad326f6fc59fa186d974e70
adityauparulekar <adityauparulekar@gmail.com>
2024-04-24 14:40:31 -0500

    itemize

--- jupyter2tex.py
+++ jupyter2tex.py
@@ -8,11 +8,10 @@ for x in all_cells:
     type = x['cell_type']
     source = [c for c in x['source']]
     all_cells_simplified.append((type, source))
-print(all_cells_simplified)
 all_markdown_lines = [y for x in parsed.get_markdown_cell_sources() for y in x.split('\n')]
 print(all_markdown_lines)
 enum_i = []
-
+item_i = []
 def count_spaces(l):
     ret = 0
     first = 0
@@ -38,16 +37,16 @@ def enumerate(line):
             if level == len(enum_i) or len(enum_i) == 0:
                 if l_num == '1':
                     enum_i.append(1)
-                    return '\\begin{enumerate} \n' + f'\item[{enum_i[-1]}.]' + l_rest
+                    return ['\\begin{enumerate} \n' + f'\item[{enum_i[-1]}.]' + l_rest]
                 else:
-                    return line
+                    return [line]
             elif level <= len(enum_i):
                 p = []
                 for i in range(level, len(enum_i)-1):
                     p.append('\\end{enumerate}\n')
                 enum_i = enum_i[:level+1]
                 enum_i[-1] += 1
-                return ''.join(p) + f'\item[{enum_i[-1]}.]' + l_rest
+                return [''.join(p), f'\item[{enum_i[-1]}.]' + l_rest]
         else:
             for i in range(len(enum_i)):
                 pre.append('\\end{enumerate}\n')
@@ -59,22 +58,40 @@ def enumerate(line):
         enum_i = []
     
     # ELSE
-    return ''.join(pre) + line
+    return [''.join(pre), line]
 def first_non_whitespace(s):
     for c in s:
         if not c.isspace():
             return c
     return -1 
-item_i = []
+
+def only_dashes(l):
+    for c in l:
+        if not (c.isspace() or c == '-'):
+            return False        
+    return True
+item_depth = -1
 def itemize(line):
-    global item_i
-    if first_non_whitespace(line) == -1 or first_non_whitespace(line) != '-':
-        item_i = 0
-        return line
-    pre = []
-    level, l = count_spaces(line)
-    if level == len(item_i):
-        pass
+    global item_depth
+    ret = []
+    if only_dashes(line) or first_non_whitespace(line) == -1 or first_non_whitespace(line) != '-':
+        for i in range(item_depth + 1):
+            ret.append("\\end{itemize} \n")
+        ret.append(line)
+        item_depth = -1
+    else:
+        level, l = count_spaces(line)
+        if level == item_depth + 1:
+            ret.append("\\begin{itemize}\n \\item " + l[1:] + "\n")
+            item_depth += 1
+        elif level == item_depth:
+            ret.append("\\item " + l[1:] + "\n")
+        else:
+            for i in range(item_depth - level):
+                ret.append("\\end{itemize} \n")
+            ret.append("\\item " + l[1:] + "\n")
+            item_depth = level
+    return ret
 
 def process_hlines(line):
     lines = line.split("\n")
@@ -106,10 +123,15 @@ def preprocess(line):
     # BOLD AND ITALIC
     line = re.sub(r'\*\*(.*?)\*\*', r'\\textbf{\1}', line)
     line = re.sub(r'\*(.*?)\*', r'\\textit{\1}', line)
-
     # ENUMERATE
-    line = enumerate(line)
+    print("BEFORE ENUMERATE", line)
+    ret = enumerate(line)
+    print("OUT OF ENUMERATE", ret)
 
+    # ITEMIZE
+    other_ret = itemize(ret[-1])
+
+    line = ''.join(ret[:-1]) + ''.join(other_ret)
     # HLINES
     line = process_hlines(line)
     # DONE
@@ -131,21 +153,24 @@ def is_part_of_special_block(line, began_math, began_verbatim):
 def markdown_to_latex(cells):
     latex_lines = ['\\documentclass{article}\n\\usepackage{graphicx}\n\\usepackage{hyperref}\n\\begin{document}']
     for cell in cells:
+        print(cell)
         if cell[0] == 'markdown':
             lines = cell[1]
             began_verbatim = False; began_math = False
-            lines = [y.strip() for x in lines for y in split_on_dollar_signs(x)]
-            lines = [y.strip() for x in lines for y in split_on_verbatim_signs(x)]
+            lines = [y for x in lines for y in split_on_dollar_signs(x)]
+            lines = [y for x in lines for y in split_on_verbatim_signs(x)]
             for line in lines:
+                print("OH NO", line)
                 if not is_part_of_special_block(line, began_math, began_verbatim):
                     line = preprocess(line)
-
+                print(line)
+                print()
                 # Convert headers
-                if line.startswith('# '):
+                if line.strip().startswith('# '):
                     latex_lines.append('\\section{' + line[2:] + '}')
-                elif line.startswith('## '):
+                elif line.strip().startswith('## '):
                     latex_lines.append('\\subsection{' + line[3:] + '}')
-                elif line.startswith('### '):
+                elif line.strip().startswith('### '):
                     latex_lines.append('\\subsubsection{' + line[4:] + '}')
                 # Convert code blocks
                 elif line == '```':
@@ -169,10 +194,14 @@ def markdown_to_latex(cells):
             for l in lines:
                 latex_lines.append(l)
             latex_lines.append("\\end{verbatim}")
+    for i in range(item_depth):
+        latex_lines.append("\\end{itemize}")
+    for i in range(len(enum_i)):
+        latex_lines.append("\\end{enumerate}")
     latex_lines.append('\\end{document}')   
     return latex_lines
 
 # RUN ON SAMPLE
 latex_lines = markdown_to_latex(all_cells_simplified)
 print(*latex_lines, sep='\n', file=open('latex.tex', 'w'))
-# os.system('pdflatex latex.tex')
+os.system('pdflatex latex.tex')
